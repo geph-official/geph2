9f6c59cfb3cd8ae17d011ff6b56475840107e26a
nullchinchilla <nullchinchilla@pm.me>
2019-11-08 11:51:08 -0500

    change timeout

--- libs/niaucchi4/wrapper.go
+++ libs/niaucchi4/wrapper.go
@@ -53,7 +53,7 @@ func (w *Wrapper) WriteTo(b []byte, addr net.Addr) (int, error) {
 	w.lock.Lock()
 	defer w.lock.Unlock()
 	if w.wire != nil {
-		if time.Since(w.lastActivity) > time.Second*3 {
+		if time.Since(w.lastActivity) > time.Second*30 {
 			w.wire.Close()
 			w.wire = nil
 			w.wire = w.getConn()
